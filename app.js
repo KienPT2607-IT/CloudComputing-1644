a1eee9b71acc9c35455e8d0ad3dc968826ddcbaf
KienPham2607 <kienptgch200815@fpt.edu.vn>
2022-10-14 20:29:30 +0700

    first initialization

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// --- Declare routers ---
+var indexRouter = require('./routes/index');
+
+var mongoose = require("mongoose");
+var db = "mongodb://localhost:27017/mykingdom_local";
+mongoose.connect(db, { useNewUrlParser: true });
+
+var app = express();
+
+// --- Config body-parser ---
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// --- Use router 
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+// --- Declare and Use port  ---
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log("Server is running http://localhost:3000");
+});
+
+module.exports = app;
